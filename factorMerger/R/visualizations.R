ec54ee91eb670b7002a1a2d78179601ab75d1927
Durszlaczek <agnieszka.sitko@student.uw.edu.pl>
2017-02-24 11:54:20 +0100

    Update generated sample plot

--- factorMerger/R/visualizations.R
+++ factorMerger/R/visualizations.R
@@ -5,16 +5,30 @@
 #' @rdname plot
 #' @importFrom ggplot2 ggplot geom_boxplot aes ylab xlab stat_summary labs
 #' @importFrom magrittr %>%
+#' @importFrom dplyr group_by summarize
 #'
 #' @export
 
 plot.generatedSample <- function(data) {
-    data %>% ggplot(aes(y = numericVec,
-                        x = factorVec,
-                        group = factorVec)) +
-        geom_boxplot() +
-        xlab("groups") + ylab("numeric") +
-        stat_summary(fun.y = mean, geom = "point", shape = 23, size = 3, fill = "violet") +
+    colnames(data) <- c("y", "x")
+    data %>% left_join(
+        data %>% group_by(x) %>% summarize(y0 = min(y),
+                                           y25 = quantile(y, 0.25),
+                                           y50 = mean(y),
+                                           y75 = quantile(y, 0.75),
+                                           y100 = max(y)), by = "x") %>%
+        ggplot(aes(y = y, x = x, group = x)) +
+        geom_boxplot(aes(ymin = y0,
+                         lower = y25,
+                         middle = y50,
+                         upper = y75,
+                         ymax = y100), stat = "identity") +
+        xlab("Groups") + ylab("Response") +
         labs(title = "Generated sample - boxplot",
-             subtitle = "Groups sorted by their means (means - violet rectangles, medians - black lines)")
+             subtitle = "Groups are sorted by means. Displayed summary statistic - mean in a group.")
+}
+
+plot.subsequentFactorMerger <- function(fm) {
+    fmMeans <- means(fm)
+    fmList <- NULL
 }
