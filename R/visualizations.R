88b1dd056d6f0621c7055cb72445cdb6edf24591
Durszlaczek <agnieszka.sitko@student.uw.edu.pl>
2017-05-03 17:45:19 +0200

    Visualizations cleaning - heatmap & profile plot

--- R/visualizations.R
+++ R/visualizations.R
@@ -14,11 +14,15 @@
 #'
 #' @export
 plot.factorMerger <- function(factorMerger, panel = "all",
-                              show = "loglikelihood", nodesSpacing = "equidistant",
+                              show = "loglikelihood",
+                              nodesSpacing = "equidistant",
                               summary = NULL, color = "none",
                               clusterSplit = list(stat = "GIC", value = 2),
                               markBestModel = TRUE, markStars = TRUE,
-                              alpha = 0.05, palette = NULL) {
+                              alpha = 0.05, penalty = 2,
+                              mergingPalette = NULL,
+                              responsePalette = NULL,
+                              GICcolor = NULL) {
 
     stopifnot(panel %in% c("all", "response", "GIC", "merging"))
     stopifnot(show %in% c("loglikelihood", "p-value"))
@@ -26,7 +30,10 @@ plot.factorMerger <- function(factorMerger, panel = "all",
     stopifnot(color %in% c("none", "cluster", "summary"))
 
     summary <- checkSummary(factorMerger, summary)
-    responsePlot <- plotResponse(factorMerger, summary, color)
+    responsePlot <- plotResponse(factorMerger, summary, color, responsePalette)
+    if(!is.null(responsePalette)) {
+        responsePlot <- ggpubr::ggpar(responsePlot, palette = responsePalette)
+    }
 
     if (panel %in% c("all", "GIC") & show == "p-value") {
         warning("GIC plot is not supported with p-value yet.")
@@ -45,8 +52,11 @@ plot.factorMerger <- function(factorMerger, panel = "all",
 
     mergingPathPlot <- plotTree(factorMerger, show, nodesSpacing,
                                 clusterSplit, markBestModel, markStars,
-                                alpha, color, colorsDf, palette)
+                                alpha, color, colorsDf, mergingPalette)
 
+    if (!is.null(mergingPalette)) {
+        mergingPathPlot <- ggpubr::ggpar(mergingPathPlot, palette = mergingPalette)
+    }
     switch(panel,
            "merging" = {
                return(mergingPathPlot)
@@ -68,13 +78,8 @@ plot.factorMerger <- function(factorMerger, panel = "all",
            })
 }
 
-checkSummary <- function(factorMerger, summary) {
-
-}
-
-plotResponse <- function(factorMerger, summary, color) {
-
-}
+# -------------------
+# Merging path plot
 
 #' @importFrom ggplot2 theme_classic theme element_line element_blank theme_minimal element_text
 treeTheme <- function() {
@@ -85,6 +90,9 @@ treeTheme <- function() {
               panel.grid.minor.y = element_blank(),
               panel.grid.minor.x = element_blank(),
               axis.title.y = element_blank(),
+              axis.text.y = element_text(size = 12),
+              plot.title = element_text(size = 18),
+              plot.subtitle = element_text(size = 12),
               legend.position = "none")
     return(myTheme)
 }
@@ -93,7 +101,7 @@ plotTree <- function(factorMerger, show, nodesSpacing,
                      clusterSplit, markBestModel, markStars,
                      alpha, color, colorsDf, palette) {
     stopifnot(show %in% c("loglikelihood", "p-value"))
-    if (nodesSpacing == "equidistance") {
+    if (nodesSpacing == "equidistant") {
         return(
             plotSimpleTree(factorMerger, show, clusterSplit,
                            markBestModel, markStars,
@@ -121,7 +129,7 @@ plotSimpleTree <- function(factorMerger, show, clusterSplit,
             paste(mH[step, ], collapse = "")
     }
     return(plotCustomizedTree(factorMerger, show, clusterSplit,
-                              "equidistance", markBestModel, markStars,
+                              "equidistant", markBestModel, markStars,
                               alpha, color, colorsDf, palette,
                               nodesPosition))
 
@@ -145,9 +153,10 @@ plotCustomizedTree <- function(factorMerger, show, clusterSplit,
     pointsDf <- segment$pointsDf
     labelsDf <- segment$labelsDf
     labelsDf <- labelsDf %>% arrange(-y1)
-    showY <- nodesSpacing != "equidistance"
+    showY <- nodesSpacing != "equidistant"
 
-    g <- df %>% ggplot() + geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2)) +
+    g <- df %>% ggplot() +
+        geom_segment(aes(x = x1, y = y1, xend = x2, yend = y2), size = 0.75) +
         geom_point(data = pointsDf, aes(x = x1, y = y1), size = 0.75)
 
     g <- g + scale_y_continuous(limits = getLimits(labelsDf, showY),
@@ -189,13 +198,15 @@ colorCluster <- function(plot, segment, factorMerger, clusterSplit, show, palett
     segmentColoured <- getClustersColors(segment, factorMerger, clusterSplit, show)
     plot <- plot +
         geom_segment(data = segmentColoured$df,
-                     aes(x = x1, y = y1, xend = x2, yend = y2, col = pred)) +
+                     aes(x = x1, y = y1, xend = x2, yend = y2, col = pred), size = 0.75) +
         geom_point(data = segmentColoured$pointsDf,
                    aes(x = x1, y = y1, col = pred), size = 0.75)
     nGroups <- length(unique(segmentColoured$labelsDf$pred))
     clusterColors <- factor(segmentColoured$labelsDf$pred, labels = hue_pal()(nGroups)) %>%
         as.character()
-    plot <- plot + theme(axis.text.y = element_text(color = clusterColors[length(clusterColors):1]))
+    plot <- plot + theme(axis.text.y = element_text(
+        color = clusterColors[length(clusterColors):1],
+        size = 12))
     return(plot)
 }
 
@@ -244,3 +255,166 @@ applyModelTransformation.default <- function(object, nodesPosition) {
     warning("Model specific nodes spacing is not supported yet.")
     return(nodesPosition)
 }
+
+# -------------------
+# Summary/response - top right plot
+
+checkSummary <- function(object, summary) {
+    UseMethod("checkSummary", object)
+}
+
+checkSummary.gaussianFactorMerger <- function(factorMerger, summary) {
+    if (NCOL(factorMerger$response) > 1) {
+        summarySet <-  c("heatmap", "profile")
+    } else {
+        summarySet <- c("means", "boxplot")
+    }
+    warnIfUnexpectedSummary(summarySet, summary)
+}
+
+checkSummary.binomialFactorMerger <- function(factorMerger, summary) {
+    summarySet <- c("proportion")
+    warnIfUnexpectedSummary(summarySet, summary)
+}
+
+checkSummary.survivalFactorMerger <- function(factorMerger, summary) {
+    summarySet <- c("survival")
+    warnIfUnexpectedSummary(summarySet, summary)
+}
+
+warnIfUnexpectedSummary <- function(summarySet, summary) {
+    if (is.null(summary)) {
+        return(summarySet[1])
+    }
+    if (!(summary %in% summarySet)) {
+        warning(paste0("Summary '", summary, ", is not supported by supplied model family -- ", summarySet[1], " used insted."))
+        return(summarySet[1])
+    }
+    return(summary)
+}
+
+plotResponse <- function(factorMerger, summary, color, palette) {
+    switch(summary,
+           "heatmap" = {
+               return(plotHeatmap(factorMerger, color))
+           },
+           "profile" = {
+               return(plotProfile(factorMerger, color))
+           },
+           "boxplot" = {
+               return(plotBoxplot(factorMerger, color))
+           },
+           "means" = {
+               return(plotMeansAndStds(factorMerger, color))
+           },
+           "survival" = {
+               return(plotSurvival(factorMerger, color))
+           },
+           "proportion" = {
+               return(plotProportion(factorMerger, color))
+           })
+}
+
+#' @importFrom proxy dist
+#' @importFrom dplyr arrange
+findSimilarities <- function(factorMerger) {
+    stats <- calculateMeansAndRanks(factorMerger$response,
+                                    factorMerger$factor)
+    varsToBePloted <- reshape(stats %>% subset(select = -mean),
+                              idvar = "level",
+                              timevar = "variable",
+                              direction = "wide")
+    distances <- dist(varsToBePloted[, -1] %>% t(), method = "manhattan")
+    iso <- MASS::isoMDS(distances, k = 1, trace = FALSE)$points[, 1]
+    iso <- data.frame(var = stats$variable %>% unique(), proj = iso) %>%
+        arrange(proj)
+    stats$variable <- factor(stats$variable,
+                             levels = iso$var)
+    return(stats)
+
+}
+
+#' Heatmap (multi-dimensional gaussian)
+#'
+#' @description Plots heatmap for each dimension of the response variable. Vector of means of factor levels for a given
+#' dimension is scaled to have mean equal to zero and standard deviation equal to one.
+#'
+#' @export
+#' @importFrom ggplot2 ggplot geom_tile aes ylab xlab stat_summary labs theme_minimal scale_x_continuous theme
+#' @importFrom ggplot2 coord_flip element_line element_blank scale_fill_distiller labs guides
+#' @importFrom magrittr %>%
+#' @importFrom reshape2 melt
+#' @importFrom dplyr filter arrange
+plotHeatmap <- function(factorMerger, color) {
+    levels <- getFinalOrderVec(factorMerger)
+    factorMerger$factor <- factor(factorMerger$factor, levels = levels)
+    df <- findSimilarities(factorMerger)
+    tmp <- tapply(df$mean, df$variable, function(x) scale(x) %>% as.numeric())
+    df$mean <- (do.call(cbind, tmp) %>% reshape2::melt())$value
+    df %>% ggplot() +
+        geom_tile(aes(x = level, y = variable, fill = mean)) +
+        coord_flip() + theme_minimal() +
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.border = element_blank(),
+              panel.background = element_blank(),
+              axis.title.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              axis.text.y = element_blank(),
+              plot.title = element_text(size = 18),
+              plot.subtitle = element_text(size = 12),
+              legend.position = "none") +
+        scale_fill_distiller(palette = "Greys") +
+        labs(title = "Heatmap", subtitle = "Group means by variables")
+}
+
+#' Profile plot (multi-dimensional gaussian)
+#'
+#' @description Plots rank plot - one series is a single factor level and one group
+#' on the OX axis is a single dimension of the response.
+#'
+#'
+#' @importFrom ggplot2 ggplot aes geom_line geom_text theme_minimal theme scale_color_manual labs
+#' @export
+plotProfile <- function(factorMerger, color) {
+    df <- findSimilarities(factorMerger)
+
+    df$group <- factor(df$level,
+                       levels = (df %>%
+                                     filter(variable == levels(df$variable) %>%
+                                                head(1)) %>% arrange(rank))$level
+    )
+
+    noLevels <- length(levels(df$level))
+    df$rank <- factor(df$rank, levels = 1:noLevels)
+
+    switch(color,
+           "cluster" = {
+               df <- df %>% left_join(getOptimalPartitionDf(factorMerger),
+                                      by = c("level" = "orig"))
+               g <- df %>% ggplot(aes(x = variable, y = rank,
+                                      colour = pred, group = group, label = group))
+           },
+           "summary" = {
+               g <- df %>% ggplot(aes(x = variable, y = rank,
+                                      col = group, group = group, label = group))
+           },
+           "none" = {
+               g <- df %>% ggplot(aes(x = variable, y = rank,
+                                      col = group, group = group, label = group)) +
+                   scale_color_manual(values = colorRamps::magenta2green(noLevels))
+           })
+
+    g <- g + geom_line(size = 1) +
+        geom_text(data = subset(df,
+                                variable == levels(df$variable) %>% tail(1)),
+                  aes(x = variable),
+                  size = 5.5, hjust = 0.8,  nudge_x = 0.5) +
+        ylab("") + labs(title = "Profile plot", subtitle = "Variable means ranks") +
+        theme_minimal() + theme(legend.position = "none",
+              plot.title = element_text(size = 18),
+              plot.subtitle = element_text(size = 12))
+    return(g)
+}
+
+
