81d0cae51f92baaca315d49d17c5c0cdf7117393
Durszlaczek <agnieszka.sitko@student.uw.edu.pl>
2017-07-30 21:04:21 +0200

    Update pisa2012 and ess data

--- R/dataDocs.R
+++ R/dataDocs.R
@@ -24,14 +24,21 @@ NULL
 
 #' PISA 2012 dataset
 #'
-#' A dataset from \url{http://www.oecd.org/pisa/} about student performance in 2012.
+#' Data from the \code{PISA2012lite} package.
+#'
+#' The original dataset (Program for International Student Assesment, \url{http://www.oecd.org/pisa/}) contains information on students'
+#' performance on various cognitive tests expressed with so-called plausible values measured in three fields: Mathematics (\code{PV1MATH}),
+#' Science (\code{PV1SCIE}) and Reading (\code{PV1READ}).
+#'
+#' \code{pisa2012} is a reweighted version of the \code{PISA2012lite} data. The weighting was performed using the \code{W_FSTUWT} variable with the seed 123.
+#'
+#' The variables are as follows (original column names from the \code{PISA2012lite} package are given in brackets):
 #'
 #' \itemize{
-#'   \item PV1MATH student's performance (Mathematics)
-#'   \item PV1READ student's performance (Reading)
-#'   \item PV1SCIE student's performance (Science)
-#'   \item CNT student's country
-#'   \item OCOD1 parent's occupation
+#'   \item \code{math} -- student's performance in Mathematics (originally: PV1MATH)
+#'   \item \code{reading} -- student's performance in Reading (originally: PV1READ)
+#'   \item \code{science} -- student's performance in Science (originally: PV1SCIE)
+#'   \item \code{country} -- student's country (originally: CNT)
 #' }
 #'
 #' @name pisa2012
@@ -43,14 +50,16 @@ NULL
 
 #' European Social Survey - happiness
 #'
-#' A dataset from \url{http://www.oecd.org/pisa/} about student performance in 2012.
+#' A dataset from \url{http://www.europeansocialsurvey.org/download.html?file=ESS7e02_1&y=2014} on hapiness
+#' of European countries.
 #'
+#' \code{ess} is a reweighted version of the original data. The weighting was performed using the \code{dweight} variable with the seed 123.
+#' The variables are as follows:
 #' \itemize{
-#'   \item happy
-#'   \item PV1READ student's performance (Reading)
-#'   \item PV1SCIE student's performance (Science)
-#'   \item CNT student's country
-#'   \item OCOD1 parent's occupation
+#'   \item \code{happy} -- binary, denoting happiness of an invidual. Interviewees were asked
+#'   to assess their happiness in the scale 0-10 (variable \code{happy}). Respondents who gave value
+#'   greater than 5 are considered as "happy",
+#'   \item \code{country} -- respondent's country.
 #' }
 #'
 #' @name ess
